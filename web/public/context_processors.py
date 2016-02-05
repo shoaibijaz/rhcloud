7848c4a9eb3a50045ba0cae13cda927dd1353107
Shoaib Ijaz <shoaib.ijaz8@gmail.com>
2016-02-06 00:04:28 +0500

    Revert "init"
    This reverts commit 0b226eb49ac6e28a01d2f85546fdf7083ea80a57.

--- web/public/context_processors.py
+++
@@ -1,6 +0,0 @@
-def settings(request):
-    """
-    Put selected settings variables into the default template context
-    """
-    from electric.global_settings import GLOBAL_SETTINGS
-    return GLOBAL_SETTINGS
